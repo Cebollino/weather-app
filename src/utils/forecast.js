5098d22d2a1a3f2d7ff35179b825fee9ebaf90a6
Cebollino <albmoto.ops@gmail.com>
2020-04-06 22:49:11 +0200

    Maximum and minimum temperature added to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location, try another search', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees celsius out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees celsius out. There is a ' + body.currently.precipProbability + '% chance of rain. The minimum temperature '+body.daily.data[0].temperatureMin+' for today is while maximum is '+body.daily.data[0].temperatureMax+'')
         }
     })
 }
